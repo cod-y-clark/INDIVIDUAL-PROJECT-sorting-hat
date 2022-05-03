23d19acf05cc5dd211abdf24d997d5cf38756bde
cod-y-clark <cody66clark@gmail.com>
2022-05-03 18:16:11 -0500

    adding intro card

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+// informative console.log
 console.log('JK Rowling is a terf');
+
+// empty array to hold students
+let students = [];
+
+// renders app to Dom (makes everything show up on the screen)
+const renderToDom = (divId, textToRender) => {
+  const selectElement = document.querySelector(divId);
+  selectElement.innerHTML = textToRender;
+};
+
+// creates form to enter student name
