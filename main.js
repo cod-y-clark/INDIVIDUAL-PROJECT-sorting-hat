594ac2403bafb3e16d49afee8e136dadb49f9de6
cod-y-clark <cody66clark@gmail.com>
2022-05-07 09:03:13 -0500

    broken sorting

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-// informative console.log
-console.log('JK Rowling is a terf');
-
 // empty string that will hold the HTML representation for the app
 let domString = ''
 
-// empty array to hold students
+// data for app
 let students = [];
+const house = ['Gryffindor', 'Hufflepuff', 'Slytherin', 'Ravenclaw']
+const randomHouse = house[Math.floor(Math.random() * house.length)]
+
+// informative console.log
+console.log(students);
 
 // renders app to Dom (makes everything show up on the screen)
 const renderToDom = (divId, textToRender) => {
@@ -22,35 +24,52 @@ const introCard = () => {
               <p class="card-text">Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss?</p>
               <hr>
               <p>Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup.</p>
-              <button id="form-button" href="#" class="btn btn-primary">Sort a new student!</button>
+              <button id="form-button" href="#" class="btn btn-primary" type="submit">Sort a new student!</button>
           </div>
         </div>`
   renderToDom('#app', domString)
-}
+};
 
 // creates the form
 const createForm = () => {
-  let domString = `<form id="form">
+  let domString = `
+                  <form id="form">
                     <h1>Enter First Year's Name</h1>
                     <div class="mb-3">
                       <label class="form-label">Student:</label>
                       <input type="text" class="form-control" id="studentNameForm" placeholder="N.K. Jemisin">
                     </div>
                     <button type="submit" class="btn btn-primary">Sort!</button>
-                  </form> `
+                  </form>`
   renderToDom('#form', domString)
-}
+};
 
-// takes action when a user interacts with the page
-const eventListeners = () => {
+// opens form
+const openForm = () => {
   // event listener to open form
   document.querySelector('#form-button').addEventListener('click', createForm);
-  }
+};
+
+const sortStudent = () => {
+  document.querySelector("#submitForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const random = Math.floor(Math.random() * house.length);
+    const newStudentObj = {
+      id: students.length + 1,
+      name: document.querySelector("#fullName").value,
+      house: houses[random],
+    };
+    students.push(newStudentObj);
+    createForm.reset();
+    console.log(students);
+  });
+};
 
 // calls the functions that renders everything to the dom
 const startApp = () => {
   introCard();
-  eventListeners();
+  openForm();
+  sortStudent();
 }
 
 startApp();
